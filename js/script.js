b0446a48f6ff149572d32c3963dd62fb1e16d762
Gooloooo <eddy_michel@hotmail.fr>
2021-06-03 16:51:54 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        nom: "",
+        prenom: "",
+        date: "",
+        bio: "",
+        hobbie1: "",
+        hobbie2: "",
+        hobbie3: "",
+        musique1: "",
+        musique2: "",
+        musique3: "",
+        bgcolor: "",
+        txtcolor: ""
+    }
+})
